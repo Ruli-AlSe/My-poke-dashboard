3f06a81c99ea340c56e183e9585d34157fb9a879
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-04 18:18:30 -0600

    Create cart counter

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,16 @@
+import { CartCounter } from "@/app/shopping-cart/components";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SEO title",
+  description: "SEO description",
+};
+
+export default function CounterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Products in cart</span>
+      <CartCounter />
+    </div>
+  );
+}
