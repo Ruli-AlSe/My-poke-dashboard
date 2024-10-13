9f58ebf4febaf5aadf4aa853346add4a7aa1096d
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-12 19:48:23 -0600

    Implement funcions to save and retrieve favorite pokemons in local storage

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -2,21 +2,19 @@
 
 import { useAppSelector } from "@/store";
 import { PokemonGrid } from "./PokemonGrid";
-import { useState } from "react";
 import { IoHeartOutline } from "react-icons/io5";
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector((state) =>
-    Object.values(state.pokemons)
+    Object.values(state.pokemons.favorites)
   );
-  const [pokemons, setPokemons] = useState(favoritePokemons);
 
   return (
     <>
-      {pokemons.length === 0 ? (
+      {favoritePokemons.length === 0 ? (
         <NoFavorites />
       ) : (
-        <PokemonGrid pokemons={pokemons} />
+        <PokemonGrid pokemons={favoritePokemons} />
       )}
     </>
   );
--- src/store/pokemons/pokemonsSlice.ts
+++ src/store/pokemons/pokemonsSlice.ts
@@ -2,32 +2,42 @@ import { SimplePokemon } from "@/pokemons";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface PokemonsState {
-  [key: string]: SimplePokemon;
+  favorites: {
+    [key: string]: SimplePokemon;
+  };
 }
 
 const initialState: PokemonsState = {
   // "1": { id: "1", name: "bulbasaur" },
   // "9": { id: "9", name: "blastoise" },
   // "38": { id: "38", name: "ninetales" },
+  // ...getInitialState(),
+  favorites: {},
 };
 
 const pokemonsSlice = createSlice({
   name: "pokemons",
   initialState,
   reducers: {
+    setFavoritePokemons(
+      state,
+      action: PayloadAction<{ [key: string]: SimplePokemon }>
+    ) {
+      state.favorites = action.payload;
+    },
     toggleFavorite(state, action: PayloadAction<SimplePokemon>) {
       const pokemon = action.payload;
       const { id } = pokemon;
-      if (!!state[id]) {
-        delete state[id];
+      if (!!state.favorites[id]) {
+        delete state.favorites[id];
 
         return;
       }
 
-      state[id] = pokemon;
+      state.favorites[id] = pokemon;
     },
   },
 });
 
-export const { toggleFavorite } = pokemonsSlice.actions;
+export const { toggleFavorite, setFavoritePokemons } = pokemonsSlice.actions;
 export default pokemonsSlice.reducer;
